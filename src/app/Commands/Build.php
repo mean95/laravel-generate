28a1276077a6ae4c0ea938ec6640691828fe7d6a
mean95 <mean.dev95@gmail.com>
2022-08-07 10:43:28 +0700

    Upadate handle build folder

--- src/app/Commands/Build.php
+++ src/app/Commands/Build.php
@@ -28,13 +28,13 @@ class Build extends Command
 	protected $toCore = 'core/';
 
 	/**
-	 * Path vendor core 
+	 * Path vendor core
 	 * @var string
 	 */
 	protected $fromCore = 'vendor/mean95/build-generate/src/installs/core';
 
 	/**
-	 * Path vendor public 
+	 * Path vendor public
 	 * @var string
 	 */
 	protected $fromPublic = 'vendor/mean95/build-generate/src/installs/public';
@@ -49,29 +49,47 @@ class Build extends Command
 		try {
 			$this->info('Build module generate started...');
 
-			$fromCore = base_path($this->fromCore);
-			$fromPublic = base_path($this->fromPublic);
-			$toCore = base_path($this->toCore);
-
-			$this->info('from: ' . $fromCore . ' to: ' . $toCore);
-
-			if ($this->confirm("You are ready for the building module generate?", true)) {
-				// Copy folder app
-				File::makeDirectory($toCore, 0777, true, true);
-				File::copyDirectory($fromCore, $toCore);
-
-				//Copy file css platform
-				$toPublic = public_path();
-                File::copyDirectory($fromPublic, $toPublic);
-
-				$this->info("\nAdmin successfully build module generate.");
-			} else {
-				$this->error("Building aborted. Please try again. Thank you...");
-			}
+			if (File::exists(base_path($this->toCore))) {
+			    $this->warn('The structure has already been built, if you do a rebuild then the previously implemented functionality will be lost.');
+			    if ($this->confirm("Are you sure you want to rebuild?", true)) {
+                    $this->buildFolder();
+                } else {
+                    $this->error("Building aborted. Please try again. Thank you...");
+                }
+            } else {
+                if ($this->confirm("You are ready for the building module generate?", true)) {
+                    $this->buildFolder();
+                } else {
+                    $this->error("Building aborted. Please try again. Thank you...");
+                }
+            }
 		} catch (Exception $e) {
 			$msg = $e->getMessage();
             $this->error('Build::handle exception: ' . $e);
             throw new Exception('Build::handle Unable to install : ' . $msg, 1);
 		}
 	}
+
+    /**
+     * Build folder generate
+     * @return void
+     */
+	protected function buildFolder()
+    {
+        $fromCore = base_path($this->fromCore);
+        $fromPublic = base_path($this->fromPublic);
+        $toCore = base_path($this->toCore);
+
+        $this->info('from: ' . $fromCore . ' to: ' . $toCore);
+
+        // Copy folder app
+        File::makeDirectory($toCore, 0777, true, true);
+        File::copyDirectory($fromCore, $toCore);
+
+        //Copy file css platform
+        $toPublic = public_path();
+        File::copyDirectory($fromPublic, $toPublic);
+
+        $this->info("\nAdmin successfully build module generate.");
+    }
 }
